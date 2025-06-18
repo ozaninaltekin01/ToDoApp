d79e979c4b085260dab276f1ab7d2d6c0f9f982d
Ozan İnaltekin <ozaninaltekin@gmail.com>
2025-06-18 18:34:48 +0300

    Sifreleme eklendi

--- routers/auth.py
+++ routers/auth.py
@@ -1,12 +1,42 @@
-from email.policy import default
-
-from fastapi import APIRouter
-
+from fastapi import APIRouter, Depends, HTTPException
+from pydantic import BaseModel
+from typing import Annotated
+from sqlalchemy.orm import Session
+from database import SessionLocal
+from models import User
+from passlib.context import CryptContext
 
 router = APIRouter()
 
-@router.get("/get_user")
-async def get_user():
-    return {"message": "User data retrieved successfully"}
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
 
+db_dependency = Annotated[Session, Depends(get_db)]
 
+bcrypt_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+class CreateUserRequest(BaseModel):
+    username: str
+    email: str
+    first_name: str
+    last_name: str
+    password: str
+    role: str
+
+@router.post("/auth")
+async def create_user(db: db_dependency,create_user_request: CreateUserRequest):
+    user = User(
+        username=create_user_request.username,
+        email=create_user_request.email,
+        first_name=create_user_request.first_name,
+        last_name=create_user_request.last_name,
+        role=create_user_request.role,
+        is_active = True,
+        hashed_password=bcrypt_context.hash(create_user_request.password)
+    )
+    db.add(user)
+    db.commit()
